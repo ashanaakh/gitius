e6dcf3f86e399f3f8bbfea6f9b4d8daa9e5a01fa
Ali Shanaakh <ashanaakh@gmail.com>
2017-10-01 17:53:53 +0300

    Initial commit

---
+++ lib/gitius.rb
@@ -0,0 +1,10 @@
+require 'thor'
+require 'octokit'
+require 'yaml'
+
+require 'gitius/version'
+require 'gitius/helpers'
+require 'gitius/error'
+
+require 'gitius/base'
+require 'gitius/core'
---
+++ lib/gitius/version.rb
@@ -0,0 +1,3 @@
+module Gitius
+  VERSION = '0.0.1'
+end
