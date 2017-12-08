e4ffeb62ba80cb087ae23218f8d899fc8f7d5af9
Ali Shanaakh <ashanaakh@gmail.com>
2017-12-08 22:02:46 +0200

    Fixes

--- lib/gitius.rb
+++ lib/gitius.rb
@@ -1,13 +1,13 @@
+require 'erb'
 require 'thor'
 require 'octokit'
 require 'yaml'
 
-require 'gitius/config'
 require 'gitius/version'
+require 'gitius/config'
 require 'gitius/helpers'
-require 'gitius/base'
 
+require 'gitius/base'
 require 'gitius/repository'
 require 'gitius/gist'
-
 require 'gitius/core'
